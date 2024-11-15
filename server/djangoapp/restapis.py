ef383b69f965eef662c2801acfe2f0a51cdf9c49
mistryvinay <vinay@symphony.com>
2024-11-15 20:43:06 +0000

    restapis.py: added linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,12 +1,15 @@
 # Uncomment the imports below before you add the function code
-# import requests
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+sentiment_analyzer_url = os.getenv(
+    'sentiment_analyzer_url', 
+    default="http://localhost:5050/"
+)
 
 
 def get_request(endpoint, **kwargs):
