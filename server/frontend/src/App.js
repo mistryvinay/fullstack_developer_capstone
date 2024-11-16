7bfac5e9b871208745aa357976419b50f8b84ace
mistryvinay <mrvinaymistry@gmail.com>
2024-11-16 08:37:33 -0500

    review additions

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,14 +1,18 @@
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
+import Dealer from "./components/Dealers/Dealer"
 import Dealers from './components/Dealers/Dealers';
+import PostReview from "./components/Dealers/PostReview";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
